97429ec0a107e78d770c3e37783b1ad22c67a75a
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-08-06 20:12:19 +0000

    add: http and json libs

--- poke-client-api/src/Pages/Home_.elm
+++ poke-client-api/src/Pages/Home_.elm
@@ -1,11 +1,67 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
-import Html
+import Html exposing (Html)
+import Page exposing (Page)
 import View exposing (View)
 
 
-page : View msg
+page : Page Model Msg
 page =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    Page.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ExampleMsgReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ExampleMsgReplaceMe ->
+            ( model
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Pages.Home_"
+    , body = [ Html.text "/" ]
     }
