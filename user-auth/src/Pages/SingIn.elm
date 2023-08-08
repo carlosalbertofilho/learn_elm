2b1f96177f09ecdeff11b52894f9108c81bd7f93
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-08-08 22:10:42 +0000

    feat: add Post do API

--- user-auth/src/Pages/SingIn.elm
+++ user-auth/src/Pages/SingIn.elm
@@ -1,9 +1,13 @@
 module Pages.SingIn exposing (Model, Msg, page)
 
+import Api.SignIn
 import Effect exposing (Effect)
-import Route exposing (Route)
-import Html
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events
+import Http
 import Page exposing (Page)
+import Route exposing (Route)
 import Shared
 import View exposing (View)
 
@@ -23,12 +27,18 @@ page shared route =
 
 
 type alias Model =
-    {}
+    { email : String
+    , password : String
+    , isSubmittingForm : Bool
+    }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
-    ( {}
+    ( { email = ""
+      , password = ""
+      , isSubmittingForm = False
+      }
     , Effect.none
     )
 
@@ -38,14 +48,45 @@ init () =
 
 
 type Msg
-    = ExampleMsgReplaceMe
+    = UserUpdatedInput Field String
+    | UserSubmittedForm
+    | SignInApiResponded (Result Http.Error Api.SignIn.Data)
+
+
+type Field
+    = Email
+    | Password
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        ExampleMsgReplaceMe ->
-            ( model
+        UserUpdatedInput Email value ->
+            ( { model | email = value }
+            , Effect.none
+            )
+
+        UserUpdatedInput Password value ->
+            ( { model | password = value }
+            , Effect.none
+            )
+
+        UserSubmittedForm ->
+            ( { model | isSubmittingForm = True }
+            , Api.SignIn.post
+                { onResponse = SignInApiResponded
+                , email = model.email
+                , password = model.password
+                }
+            )
+
+        SignInApiResponded (Ok { token }) ->
+            ( { model | isSubmittingForm = False }
+            , Effect.none
+            )
+
+        SignInApiResponded (Err httpError) ->
+            ( { model | isSubmittingForm = False }
             , Effect.none
             )
 
@@ -65,6 +106,102 @@ subscriptions model =
 
 view : Model -> View Msg
 view model =
-    { title = "Pages.SingIn"
-    , body = [ Html.text "/sing-in" ]
+    { title = "Login"
+    , body = [ viewPage model ]
     }
+
+
+viewPage : Model -> Html Msg
+viewPage model =
+    Html.div
+        [ Attr.class "columns is-mobile is-centered" ]
+        [ Html.div
+            [ Attr.class "column is-narrow" ]
+            [ Html.div
+                [ Attr.class "section" ]
+                [ Html.h1
+                    [ Attr.class "title" ]
+                    [ Html.text "Login"
+                    , viewForm model
+                    ]
+                ]
+            ]
+        ]
+
+
+viewForm : Model -> Html Msg
+viewForm model =
+    Html.form
+        [ Attr.class "box"
+        , Html.Events.onSubmit UserSubmittedForm
+        ]
+        [ viewFormInput
+            { field = Email
+            , value = model.email
+            }
+        , viewFormInput
+            { field = Password
+            , value = model.password
+            }
+        , viewFormControls model
+        ]
+
+
+viewFormInput :
+    { field : Field
+    , value : String
+    }
+    -> Html Msg
+viewFormInput options =
+    Html.div
+        [ Attr.class "field" ]
+        [ Html.label
+            [ Attr.class "label" ]
+            [ Html.text (fromFieldToLabel options.field) ]
+        , Html.div
+            [ Attr.class "control" ]
+            [ Html.input
+                [ Attr.class "input"
+                , Attr.type_ (fromFieldToInputType options.field)
+                , Attr.value options.value
+                , Html.Events.onInput (UserUpdatedInput options.field)
+                ]
+                []
+            ]
+        ]
+
+
+fromFieldToLabel : Field -> String
+fromFieldToLabel field =
+    case field of
+        Email ->
+            "Email address"
+
+        Password ->
+            "Password"
+
+
+fromFieldToInputType : Field -> String
+fromFieldToInputType field =
+    case field of
+        Email ->
+            "Email address"
+
+        Password ->
+            "Password"
+
+
+viewFormControls : Model -> Html Msg
+viewFormControls model =
+    Html.div
+        [ Attr.class "field is-grouped is-grouped-right" ]
+        [ Html.div
+            [ Attr.class "control" ]
+            [ Html.button
+                [ Attr.class "button is-link"
+                , Attr.disabled model.isSubmittingForm
+                , Attr.classList [ ( "is-loading", model.isSubmittingForm ) ]
+                ]
+                [ Html.text "Sign in" ]
+            ]
+        ]
