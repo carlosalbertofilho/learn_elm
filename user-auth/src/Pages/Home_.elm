dd4d167f642c8f12c8708aea8f29213de11ce007
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-08-11 19:34:50 +0000

    feat: Apply layout on Pages

--- user-auth/src/Pages/Home_.elm
+++ user-auth/src/Pages/Home_.elm
@@ -3,19 +3,36 @@ module Pages.Home_ exposing (Model, Msg, page)
 import Auth
 import Effect exposing (Effect)
 import Html
+import Layouts
 import Page exposing (Page)
 import Route exposing (Route)
 import Shared
 import View exposing (View)
 
 
-page : Auth.User -> Shared.Model -> Route () -> Page Model Msg
+page :
+    Auth.User
+    -> Shared.Model
+    -> Route ()
+    -> Page Model Msg
 page user shared route =
     Page.new
         { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
+        }
+        |> Page.withLayout (toLayout user)
+
+
+toLayout :
+    Auth.User
+    -> Model
+    -> Layouts.Layout Msg
+toLayout user model =
+    Layouts.Sidebar
+        { title = "Dashboard"
+        , user = user
         }
 
 
--- user-auth/src/Pages/Settings.elm
+++ user-auth/src/Pages/Settings.elm
@@ -3,19 +3,36 @@ module Pages.Settings exposing (Model, Msg, page)
 import Auth
 import Effect exposing (Effect)
 import Html
+import Layouts
 import Page exposing (Page)
 import Route exposing (Route)
 import Shared
 import View exposing (View)
 
 
-page : Auth.User -> Shared.Model -> Route () -> Page Model Msg
+page :
+    Auth.User
+    -> Shared.Model
+    -> Route ()
+    -> Page Model Msg
 page user shared route =
     Page.new
         { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
+        }
+        |> Page.withLayout (toLayout user)
+
+
+toLayout :
+    Auth.User
+    -> Model
+    -> Layouts.Layout msg
+toLayout user model =
+    Layouts.Sidebar
+        { title = "Settings"
+        , user = user
         }
 
 
